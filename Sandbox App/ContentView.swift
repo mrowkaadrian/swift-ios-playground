3a75ada643f7219f338aeb029e7eb66a9c0e04da
Adrian Mrówka <mrowkaadrian@gmail.com>
2025-01-16 21:40:52 +0100

    Created "CardView"

--- Sandbox App/ContentView.swift
+++ Sandbox App/ContentView.swift
@@ -10,12 +10,46 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            CardView(emoji: "😎", isSelected: true)
+            CardView(isSelected: true)
         }
         .padding()
+    }
+}
+
+struct CardView: View {
+    var emoji: String = "😀"
+    var text: String = "Card"
+    var isSelected: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isSelected {
+                Rectangle()
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                Rectangle()
+                    .strokeBorder(.black, lineWidth: 4)
+                    .cornerRadius(8)
+                
+                VStack {
+                    Text(emoji)
+                        .font(.largeTitle)
+                    
+                    Text(text)
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .padding()
+                }
+            } else {
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .cornerRadius(8)
+                Rectangle()
+                    .strokeBorder(.black, lineWidth: 4)
+                    .cornerRadius(8)
+            }
+        }
     }
 }
 
