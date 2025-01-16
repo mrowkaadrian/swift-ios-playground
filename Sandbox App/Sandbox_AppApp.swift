2652fb0a04e8794d05c1acdc242e8db55d595254
Adrian Mrówka <mrowkaadrian@gmail.com>
2025-01-16 16:32:01 +0100

    Initial Commit

---
+++ Sandbox App/Sandbox_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sandbox_AppApp.swift
+//  Sandbox App
+//
+//  Created by Adrian Mrówka on 16/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Sandbox_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
